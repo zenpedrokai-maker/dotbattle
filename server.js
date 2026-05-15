985659b3d10170bf8f2a3cf640ef1a88b6a6748d
Pedro Henrique <zenpedrokai@gmail.com>
2026-05-15 19:23:02 -0300

    corrigindo rota lobby

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
 
-app.use(express.static("public"));
+app.use("/public", express.static("public"));
 
 app.get("/", (req, res) => {
 
